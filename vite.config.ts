c7a7b5e1797b6b9dceefe1eb9c1e7a5dc6667d25
Jagruthi-31 <94822123+Jagruthi-31@users.noreply.github.com>
2026-08-16 16:48:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/BookLibrary/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   optimizeDeps: {
     exclude: ["pdfjs-dist"],
   },
+
   worker: {
     format: "es",
   },
